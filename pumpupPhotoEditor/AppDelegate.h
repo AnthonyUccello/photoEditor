dfe5ba14eb725af5cf68cb4f53b9840484642f25
Anthony Uccello <anthonyuccello@Anthonys-MacBook-Pro.local>
2014-09-30 12:29:40 -0400

    Initial Commit

---
+++ pumpupPhotoEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pumpupPhotoEditor
+//
+//  Created by Anthony Uccello on 2014-09-30.
+//  Copyright (c) 2014 pumpup inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pumpupPhotoEditor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pumpupPhotoEditor
+//
+//  Created by Anthony Uccello on 2014-09-30.
+//  Copyright (c) 2014 pumpup inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
